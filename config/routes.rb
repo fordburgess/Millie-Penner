7fd60db19d3b90b9ea781b6711a0ff245abcbc1b
Ford Burgess <ford.burgess@abintus.consulting>
2024-04-10 17:36:19 +0200

    order model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :zines, only: [:index, :show]  
-  resources :carts, only: [:show]
+  resources :carts, only: [:show]  
+  resources :orders
 
   root to: 'pages#home'
 
+  # cart functionality
   post '/add_to_cart/:product_id' => 'carts#add_to_cart', :as => 'add_to_cart'
   delete '/remove_from_cart/:product_id' => 'carts#remove_from_cart', :as => 'remove_from_cart'
   delete '/empty_cart' => 'carts#empty_cart', :as => 'empty_cart'
+
+  # order functionality
 
   namespace :admin do 
     root to: 'admin#dashboard'
     get '/zines/all', to: 'admin#zines_all'
     resources :zines, except: [:show, :index]
+    resources :orders, only: [:show, :index]
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_113311) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_151036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_113311) do
   create_table "carts_zines", id: false, force: :cascade do |t|
     t.bigint "cart_id", null: false
     t.bigint "zine_id", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "product"
+    t.string "address"
+    t.string "pay_method"
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
