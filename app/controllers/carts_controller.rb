17894a97a67e465600a18324d66ab109d67b5342
Ford Burgess <ford.burgess@abintus.consulting>
2024-04-16 19:59:45 +0200

    add to cart javascript works

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,23 +1,16 @@
 class CartsController < ApplicationController
     def show
-        @cart = current_cart
-        render 'cart/show'
+        @cart = current_cart        
+        render json: @cart.zines
     end
 
     def add_to_cart 
-        current_cart.add_item(params[:product_id])
-
-        respond_to do|format|
-            format.json { render json: current_cart.zines }
-        end
+        current_cart.add_item(params[:product_id])       
     end
 
-    def remove_from_cart
-        current_cart.remove_item(params[:product_id])
-
-        respond_to do|format|
-            format.json { render json: current_cart.zines }
-        end
+    def remove_from_cart        
+        current_cart.remove_item(params[:product_id])    
+        head :no_content 
     end
 
     def empty_cart
--- app/javascript/update_cart.js
+++ app/javascript/update_cart.js
@@ -1,7 +1,56 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     var addButton = document.getElementById('add-to-cart-button');
+document.addEventListener('turbo:load', function () {
+    var addButton = document.getElementById('add-to-cart-button');
+    var cartItems = document.getElementById('cart-items');
+    var zineId = addButton.getAttribute('data-zine-id');
 
-//     addButton.addEventListener('click', () => {
-//         console.log("Helo")
-//     })
-// })
+    addButton.addEventListener('click', () => {        
+        fetch(`/zines/ajax/${zineId}`, {
+            method: 'GET',
+            headers: { 'Content-Type' : 'application/json' }
+        })
+        .then((res) => {
+            if (res.ok) {                
+                return res.json()
+            }
+            else {
+                throw new Error("error")
+            }
+        })
+        .then((data) => {
+            var zine = data.zine;
+            var coverImage = data.cover_image            
+
+            var newElement = document.createElement("div");
+            var newElementContent = `
+                <div class="flex text-white mb-6 relative">
+                    <img src="${coverImage}" class="mr-4" style="max-height: 10rem; max-width: 5rem" />             
+                    <div class="font-kameron">
+                        <h2 class="mt-0 mb-4">${zine.title}</h2>
+                        <p>Price: £${(parseFloat(zine.price)).toFixed(2)}</p>
+                    </div>                    
+                </div>
+            `
+
+            newElement.innerHTML = newElementContent;
+            cartItems.appendChild(newElement);            
+        })
+    })
+
+    var deleteButton = document.getElementById('delete-button');
+    var zineIdDelete = deleteButton.getAttribute('data-zine-id');            
+    
+    // cartItems.addEventListener('click', (e) => {
+    //     e.preventDefault();
+
+    //     // if (e.target.id == 'delete-button') {
+    //     //     var zineToRemove = document.getElementById(`zine-${zineIdDelete}`);                   
+              
+    //     //     cartItems.removeChild(zineToRemove);
+
+    //     //     // fetch(`/remove_from_cart/${zineId}`, {
+    //     //     //     method: 'DELETE',
+    //     //     //     headers: { 'Content-Type' : 'application/json' }
+    //     //     // })            
+    //     // }
+    // })
+})
