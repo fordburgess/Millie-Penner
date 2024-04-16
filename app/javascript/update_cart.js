55b150fbfa60a0c5e201d7e60a376e087b5a5657
Ford Burgess <ford.burgess@abintus.consulting>
2024-04-16 10:33:51 +0200

    cart will not display if jquery is imported

--- app/javascript/update_cart.js
+++ app/javascript/update_cart.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var addButton = document.getElementById('add-to-cart-button');
+// document.addEventListener('DOMContentLoaded', function () {
+//     var addButton = document.getElementById('add-to-cart-button');
 
-    addButton.addEventListener('click', () => {
-        console.log("Helo")
-    })
-})
+//     addButton.addEventListener('click', () => {
+//         console.log("Helo")
+//     })
+// })
