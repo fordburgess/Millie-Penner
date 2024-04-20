317b0dfdf1c326a21ccd91ca131a6aa134a6671f
Ford Burgess <ford.burgess@abintus.consulting>
2024-04-20 14:05:10 +0200

    order creation works

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,13 +20,14 @@ class OrdersController < ApplicationController
   end
 
   # POST /orders or /orders.json
-  def create
+  def create    
     @order = Order.new(order_params)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
-        format.json { render :show, status: :created, location: @order }
+        head :no_content
+        # format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
+        # format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @order.errors, status: :unprocessable_entity }
@@ -38,8 +39,9 @@ class OrdersController < ApplicationController
   def update
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
+        head :no_content
+        # format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
+        # format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @order.errors, status: :unprocessable_entity }
@@ -65,6 +67,7 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:name, :email, :product, :address, :pay_method, :total)
+      # params.require(:order).permit(:name, :email, :product, :address, :pay_method, :total)
+      params.require(:order).permit(:name, :email, :address)
     end
 end
