6874b6991f2b612119a85668126a046fe418651d
Ford Burgess <ford.burgess@abintus.consulting>
2024-04-26 15:14:40 +0200

    added pdf url to zine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_152516) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_26_123147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_22_152516) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pdf_url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
