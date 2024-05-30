51add9cdbcf3bf957a41bc9d93c893cff96cd269
Ford Burgess <ford.burgess@abintus.consulting>
2024-05-30 16:28:56 +0100

    changed mua mobile video to gif

--- db/schema.rb
+++ db/schema.rb
@@ -111,13 +111,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_190451) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
